be2e2397505f96a03b0ae5dd1a7103cf1cbe0130
annamoyosore <annamoyosore@gmail.com>
2026-04-19 22:43:15 +0100

    Update

---
+++ api/send.js
@@ -0,0 +1,66 @@
+export default async function handler(req, res) {
+
+if (req.method !== "POST") {
+return res.status(405).json({ success: false, message: "Method not allowed" });
+}
+
+const {
+name,complaint
+} = req.body || {};
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+
+// Validate config
+if (!BOT_TOKEN || !CHAT_ID) {
+return res.status(500).json({
+success: false,
+message: "Missing BOT_TOKEN or CHAT_ID"
+});
+}
+
+const message = `
+📥 NEW APPLICATION
+
+👤 Name: ${name || "-"}
+🏠 complaint: ${complaint|| "-"}
+🏡 Housing: ${housing || "-"}
+`;
+
+try {
+
+const telegramRes = await fetch(
+`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+{
+method: "POST",
+headers: {
+"Content-Type": "application/json"
+},
+body: JSON.stringify({
+chat_id: CHAT_ID,
+text: message
+})
+}
+);
+
+const telegramData = await telegramRes.json();
+
+// Validate Telegram response
+if (!telegramData.ok) {
+console.log("Telegram error:", telegramData);
+return res.status(500).json({
+success: false,
+message: "Telegram API failed"
+});
+}
+
+return res.status(200).json({ success: true });
+
+} catch (error) {
+console.log("Server error:", error);
+return res.status(500).json({
+success: false,
+message: "Internal server error"
+});
+}
+}
